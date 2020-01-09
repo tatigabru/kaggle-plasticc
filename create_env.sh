04939ec2715f85c1147c9659ce5c0bad266fea74
tatigabru <travellingtati@gmail.com>
2020-01-09 02:21:41 +0000

    added torch

--- create_env.sh
+++ create_env.sh
@@ -3,6 +3,7 @@ conda update -n base -c defaults conda
 conda create -y -n plasticc python=3.6
 conda activate plasticc
 
+conda install -y -n rsna pytorch=0.4.1 cuda90 -c pytorch
 pip install --upgrade pip
 pip install -r requirements.txt
 
